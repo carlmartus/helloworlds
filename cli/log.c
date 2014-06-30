9c7bde556d3533adb6bd61b208f2ad2a2841b3b9
Martin Sandgren <carlmartus@gmail.com>
2014-06-30 14:15:41 +0200

    Add: Fancy logging with output coloring.

---
+++ cli/log.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef enum {
+	LOG_INFO,
+	LOG_WARN,
+	LOG_ERRO,
+} LogCode;
+
+static void
+log(LogCode code, const char *fmt, ...)
+{
+	const char *prefix = NULL;
+	switch (code) {
+		case LOG_INFO : prefix = "\033[92minfo"; break;
+		case LOG_WARN : prefix = "\033[93mwarn"; break;
+		default :
+		case LOG_ERRO : prefix = "\033[91merro"; break;
+	}
+
+	printf(" (%s\033[0m) : ", prefix);
+
+	va_list args;
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	va_end(args);
+
+	putchar('\n');
+}
+
+int
+main(int argc, char **argv)
+{
+	log(LOG_INFO, "Starting program");
+	log(LOG_ERRO, "Only using %d parameters", argc);
+	return 0;
+}
+
