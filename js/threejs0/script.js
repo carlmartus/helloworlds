db11d1475975d98a43cff325c499fe066517082c
Martin Sandgren <carlmartus@gmail.com>
2014-09-16 16:51:36 +0200

    Fix: Typo.

--- js/threejs0/script.js
+++ js/threejs0/script.js
@@ -1,13 +1,15 @@
 function init() {
-	var scene = new three.scene();
-	var camera = new three.perspectivecamera(75, window.innerwidth/window.innerheight, 0.1, 1000);
-	var renderer = new three.webglrenderer();
+	var scene = new THREE.scene();
+	var camera = new THREE.perspectivecamera(75, window.innerwidth/window.innerheight, 0.1, 1000);
+	var renderer = new THREE.webglrenderer();
 	renderer.setsize(window.innerwidth, window.innerheight);
 	document.body.appendchild(renderer.domelement);
-	var geometry = new three.boxgeometry(1,1,1);
-	var material = new three.meshbasicmaterial({color: 0x00ff00});
-	var cube = new three.mesh(geometry, material);
-	scene.add(cube); camera.position.z = 5;
+	var geometry = new THREE.boxgeometry(1,1,1);
+	var material = new THREE.meshbasicmaterial({color: 0x00ff00});
+	var cube = new THREE.mesh(geometry, material);
+	scene.add(cube);
+	camera.position.z = 5;
+
 	var render = function () {
 		requestanimationframe(render);
 		cube.rotation.x += 0.1;
