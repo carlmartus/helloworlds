a35b4680f8a0a25c3c6961d3ab150b5197352bee
Martin Sandgren <martin.sandgren@trivector.se>
2016-04-06 14:04:23 +0200

    feat: Generate names from random numbers

--- haskell/names.hs
+++ haskell/names.hs
@@ -1,8 +1,45 @@
 import System.Random
+import Data.List
+
+type Part = String
+
+part1 :: [Part]
+part1 = [
+    "Pol",
+    "Sand",
+    "Wester",
+    "Karl",
+    "Gustav",
+    "Nils",
+    "Lund" ]
+
+part2 :: [Part]
+part2 = [
+    "hem",
+    "man",
+    "sen",
+    "sson",
+    "gren" ]
+
+count :: Int
+count = 10 * (length part1 + length part2)
+
+
+partFromList :: [String] -> Int -> String
+partFromList list id = list !! (id `mod` length list)
+
+nameFromIds :: Int -> Int -> String
+nameFromIds id1 id2 = (partFromList part1 id1) ++ (partFromList part2 id2)
 
 main :: IO ()
 main = do
-    g <- getStdGen
-    map show $ take 2 g
-    putStrLn "Hej"
+    gen1 <- getStdGen
+    gen2 <- getStdGen
+
+    let list1 = map (`mod` length part1) (randoms gen1 :: [Int])
+    let list2 = map (`mod` length part2) (drop count $ randoms gen2 :: [Int])
+
+    let q = zip (take count list1) (take count list2)
+    let r = map (uncurry nameFromIds) q
+    mapM_ putStrLn $ (nub . sort) r
 
